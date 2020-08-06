349251941e9e2a678f0edaf6fc00cc8fe1cbcab0
3Samourai <68392445+3Samourai@users.noreply.github.com>
2020-08-06 11:31:58 +0200

    Add background.js

---
+++ background.js
@@ -0,0 +1,20 @@
+ browser.contextMenus.create({
+    id: "Translate_Selection",
+    title: "Translate the Selection",
+    contexts: ["selection"]
+  });
+  
+
+  browser.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId == "Translate_Selection") {
+
+     var url1 = "https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=" + info.selectionText;
+  //console.log(url1);
+     var creating = browser.windows.create({
+    url: url1,
+    type: "popup",
+    height: 652, 
+    width: 375
+  })
+    }
+  })
