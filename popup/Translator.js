847f799bf8077905123d020107a601c20e5a9fed
3Samourai <68392445+3Samourai@users.noreply.github.com>
2020-08-10 16:49:32 +0200

    Update Translator.js

--- popup/Translator.js
+++ popup/Translator.js
@@ -7,10 +7,27 @@ function callback(tabs) {
     url:result+currentTab.url
   });
 };
+function callBackground(tabs) {
+  var currentTab = tabs[0]; 
+  console.log(currentTab.url);
+  var result = "https://translate.google.com/translate?sl=auto&u=" 
+  console.log(result);
+  browser.runtime.sendMessage({
+    url:result+currentTab.url
+  });
+}
+
 function send()
 {
+  var lfckv = document.getElementById("toggle").checked
+  console.log(lfckv);
+ if (lfckv == true){
   var query = { active: true, currentWindow: true };
   browser.tabs.query(query, callback);
-  console.log(query);
+ }
+ else{
+  var query = { active: true, currentWindow: true };
+  browser.tabs.query(query, callBackground);
+ }
 };
 document.querySelector("#btn").onclick = send;
