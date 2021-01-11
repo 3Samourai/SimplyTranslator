198c16e8c0418f3d8b1205e65b15f82e8fb376c0
3Samourai <68392445+3Samourai@users.noreply.github.com>
2021-01-11 13:05:54 +0100

    Update Translator.js

--- popup/Translator.js
+++ popup/Translator.js
@@ -1,17 +1,17 @@
+
+var ToggleDark =  document.querySelector("#ToggleDark");
+var toggle =  document.getElementById("toggle");
+
 function callback(tabs) {
   var currentTab = tabs[0]; 
-  console.log(currentTab.url);
   var result = "https://translate.google.com/translate?sl=auto&u=" 
-  console.log(result);
   var creating = browser.tabs.create({
     url:result+currentTab.url
   });
 };
 function callBackground(tabs) {
   var currentTab = tabs[0]; 
-  console.log(currentTab.url);
   var result = "https://translate.google.com/translate?sl=auto&u=" 
-  console.log(result);
   browser.runtime.sendMessage({
     url:result+currentTab.url
   });
@@ -20,7 +20,6 @@ function callBackground(tabs) {
 function send()
 {
   var lfckv = document.getElementById("toggle").checked
-  console.log(lfckv);
  if (lfckv == true){
   var query = { active: true, currentWindow: true };
   browser.tabs.query(query, callback);
@@ -31,3 +30,86 @@ function send()
  }
 };
 document.querySelector("#btn").onclick = send;
+
+/*Dark Mode and Preferences*/
+var zero = 0;
+
+window.onload = function() {
+let gettingZero = browser.storage.local.get();
+gettingZero.then(onGot, onError);
+}
+
+function ToggleDarkMode () {
+let gettingItem = browser.storage.local.get();
+gettingItem.then(onGet, onError); 	
+}
+
+
+ToggleDark.onclick = ToggleDarkMode;
+toggle.onchange = ToggleChange;
+
+function ToggleChange(){
+	if (toggle.checked == true){
+		 browser.storage.local.set({
+      ToggleOn: true   
+         });
+	}
+	else if (toggle.checked == false){
+		 browser.storage.local.set({
+      ToggleOn: false 
+         });
+	}
+}
+
+function onGot(item) {
+	var icon = document.querySelector("#icon");
+		   var element = document.querySelector("#body");
+
+	if (Object.keys(item).length === 0 && item.constructor === Object){
+      browser.storage.local.set({
+      ModeOn: false,
+      ToggleOn: false    
+         });
+  	    	  icon.src = "sun.svg";
+  	    	 element.classList.remove("dark-mode");
+
+
+		}
+		else {
+  if (item["ModeOn"] == true){
+	  icon.src = "moon.svg";
+   element.classList.toggle("dark-mode");
+
+  }
+    else if (item["ModeOn"] == false){
+	    	  icon.src = "sun.svg";
+  	    	 element.classList.remove("dark-mode");
+  };
+  if (item["ToggleOn"] == true){
+	  toggle.checked = true;
+
+  }
+  else if (item["ToggleOn"] == false){
+	  toggle.checked = false;
+}
+}}
+function onGet (item) {
+	  	var icon = document.querySelector("#icon");
+   if (item["ModeOn"] == true){
+	   	  icon.src = "sun.svg";
+  browser.storage.local.set({
+  ModeOn: false      
+  }); }
+  else if (item["ModeOn"] == false){
+	  	  icon.src = "moon.svg";
+ browser.storage.local.set({
+  ModeOn: true     
+  });
+  }
+var element = document.querySelector("#body");
+   element.classList.toggle("dark-mode");
+
+}
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
