edcc302e1a4dd3ce30ace7c9525c17c83ebc795c
3Samourai <68392445+3Samourai@users.noreply.github.com>
2021-02-11 14:39:20 +0100

    Add Translate_Web Menu

--- background.js
+++ background.js
@@ -3,13 +3,14 @@
     title: "Translate the Selection",
     contexts: ["selection"]
   });
-  
+  browser.contextMenus.create({
+    id: "Translate_Web",
+    title: "Translate this Website"
+  });
 
   browser.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId == "Translate_Selection") {
-
      var url1 = "https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=" + info.selectionText;
-  //console.log(url1);
      var creating = browser.windows.create({
     url: url1,
     type: "popup",
@@ -17,10 +18,18 @@
     width: 375
   })
     }
+    else if (info.menuItemId == "Translate_Web"){
+      var url = "https://translate.google.com/translate?sl=auto&u=";
+      browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
+        let tab = tabs[0]; 
+        browser.tabs.update({
+          url: url + tab.url
+          });
+    }, console.error); 
+    }
   })
   function SaveMessage(message, sender)
   {
-    console.log(message.url);
     browser.tabs.update({
     url: message.url
     })
