5d681a4dd295b8cc78ea2da9dac904007d14f857
3Samourai <68392445+3Samourai@users.noreply.github.com>
2021-06-28 12:22:46 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-    browser.contextMenus.create({
+  browser.contextMenus.create({
     id: "Translate_Selection",
     title: "Translate the Selection",
     contexts: ["selection"]
@@ -9,15 +9,22 @@
     contexts: ["page"]
   });
 
+  var Translate_Selection_Win_Id;
+
   browser.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId == "Translate_Selection") {
      var url1 = "https://translate.google.com/#view=home&op=translate&sl=auto&text=" + info.selectionText;
+     if (Translate_Selection_Win_Id != undefined){
+      browser.windows.remove(Translate_Selection_Win_Id);
+     }
      var creating = browser.windows.create({
     url: url1,
     type: "popup",
     height: 652, 
     width: 375
-  })
+  }).then((data) => {
+    Translate_Selection_Win_Id = data.id;
+      });
     }
     else if (info.menuItemId == "Translate_Web"){
       var url = "https://translate.google.com/translate?sl=auto&u=";
@@ -43,11 +50,13 @@
   }
   function onGotLocal(item) {
     let urlDownloadPage = "./download.html";
+    console.log("test4")
 
     if (item["DownloadPage"] != true){
       browser.storage.local.set({
         DownloadPage: true
       });
+      console.log("test")
 
       var page = browser.tabs.create({
         url:urlDownloadPage
@@ -55,6 +64,7 @@
   }
 }
 window.onload = function() {
+  console.log("test3")
 
   let gettingThanks = browser.storage.local.get();
   gettingThanks.then(onGotLocal, onErrorLocal);
